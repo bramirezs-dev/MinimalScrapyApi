16a1f1c49b21f1bd0cd3406c969922307cf59bfe
brian.ramirez <brian.ramirez@overactive.com>
2023-02-28 14:39:36 -0500

    add get HttpClient and HtmlAgilityPack package

--- Program.cs
+++ Program.cs
@@ -4,3 +4,12 @@ var app = builder.Build();
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
+
+
+static Task<string> getToHtml(string url)
+{
+
+    var client = new HttpClient();
+    var response = client.GetStringAsync(url);
+    return response;
+}
